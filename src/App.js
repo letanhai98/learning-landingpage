f0455a7d38b1bf6c8c432ff9711f7d819478e3d4
nlthinh <windyzero2305@gmail.com>
2023-07-14 10:35:28 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='wrapper'>
+      <header className='header'>
+        <div className='container'>
+          <div className='header-container'>
+            <img src='/Logo.png' className='header-logo'></img>
+            <ul>
+              <li>
+                <a href='#' className='menulink'>About</a>
+              </li>
+            </ul>
+            <ul>
+              <li>
+                <a href='#' className='menulink'>Features</a>
+              </li>
+            </ul>
+            <ul>
+              <li>
+                <a href='#' className='menulink'>Pricing</a>
+              </li>
+            </ul>
+            <ul>
+              <li>
+                <a href='#' className='menulink'>Testimonials</a>
+              </li>
+            </ul>
+            <ul>
+              <li>
+                <a href='#' className='menulink'>Help</a>
+              </li>
+            </ul>
+            <div className='header-user'>
+            <a href='#' className='signin'>Singin</a>
+            <a href='#' className='signup'>Singup</a>
+            </div>
+          </div>
+
+        </div>
       </header>
     </div>
   );
